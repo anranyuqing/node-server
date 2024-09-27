58206d8fd76fb6a66ddabc8e0f8abc5db22ea391
lixw <GyTemp13579>
2024-09-27 20:50:18 +0800

    修改登录接口

--- src/router/index.js
+++ src/router/index.js
@@ -7,40 +7,32 @@ router.post('/login', (req, res) => {
     const { account, password } = req.body
     connection.query('select * from user', (err, result) => {
         if (err) {
-            console.log('查询失败', err)
-            res.send({
-                code: 500,
+            res.status(500).send({
                 message: '操作失败'
             })
             return
-        } else {
-            const target = result.find(el => el.account === account)
-            if (target) {
-                console.log('target', target.password, password)
-                if (target.password === password) {
-                    const token = createToken(account, password)
-                    res.send({
-                        code: 200,
-                        message: '登录成功',
-                        data: {
-                            token
-                        }
-                    })
-                } else {
-                    res.send({
-                        code: 500,
-                        message: '密码错误',
-                        data: null
-                    })
-                }
-            } else {
-                res.send({
-                    code: 200,
-                    message: '用户不存在',
-                    data: null
-                })
-            }
         }
+        const target = result.find(el => el.account === account)
+
+        if (!target) {
+            return res.send({
+                code: 200,
+                message: '用户不存在'
+            })
+        }
+
+        if (target.password !== password) {
+            return res.send({
+                code: 200,
+                message: '密码错误'
+            })
+        }
+
+        res.send({
+            message: '用户不存在',
+            data: null
+        })
+
     })
 })
 
